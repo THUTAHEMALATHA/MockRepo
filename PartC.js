87ed9f558dbf08b5cb1d504ef23dd1e8c48e3095
THUTAHEMALATHA <thutahemalatha@gmail.com>
2025-11-23 20:10:24 +0530

    Closures

--- PartC.js
+++ PartC.js
@@ -1,1 +1,31 @@
-// 1.
+// 10.
+function counter(){
+    let count=1;
+    return function(){
+    return count++;
+   }
+}
+const c = counter();
+console.log(c()); // 1
+console.log(c()); // 2
+console.log(c()); // 3
+
+// 11.
+function createWallet(){
+    let balance =0;
+    return {
+     addMoney : function(amount){ 
+            balance += amount;
+        },
+     checkBalance : function(){
+        return balance;
+     }   
+    };
+}
+
+let myWallet = createWallet();
+myWallet.addMoney(500);
+myWallet.addMoney(200);
+myWallet.checkBalance();  // should show 700
+
+
